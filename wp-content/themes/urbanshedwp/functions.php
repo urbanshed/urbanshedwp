2df0d29b85679fba4a659f76c41bed137fb4c78d
Jodie Alaine Parker <me@jodiealaine.com>
2016-04-21 18:26:40 +0100

    Add respond.js to help with IE 8 support

--- wp-content/themes/urbanshedwp/functions.php
+++ wp-content/themes/urbanshedwp/functions.php
@@ -129,6 +129,21 @@ function urbanshedwp_scripts() {
 add_action( 'wp_enqueue_scripts', 'urbanshedwp_scripts' );
 
 /**
+ * Add Respond.js for IE
+ */
+if( !function_exists('ie_scripts')) {
+	function ie_scripts() {
+	 	echo '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->';
+	   	echo ' <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->';
+	   	echo ' <!--[if lt IE 9]>';
+	    echo ' <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>';
+	    echo ' <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
+	   	echo ' <![endif]-->';
+   	}
+   	add_action('wp_head', 'ie_scripts');
+} // end if
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
